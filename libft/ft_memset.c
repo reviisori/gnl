d7e2408df6a934afef19c8fec019b646dec19c44
Aleksandr Tikka <altikka@c2r1p2.hive.fi>
2021-12-23 14:22:06 +0200

    Fixed compiler error in memset. Removed -Wconv. flag from Make

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 14:47:27 by altikka           #+#    #+#             */
-/*   Updated: 2021/12/23 13:36:54 by altikka          ###   ########.fr       */
+/*   Updated: 2021/12/23 13:52:28 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,13 @@ void	*ft_memset(void *b, int c, size_t len)
 		*(long *) ptr_l++ = *(long *) arr;
 		len -= sizeof(long) * 4;
 	}
-	ptr_i = ptr_l;
+	ptr_i = (unsigned int *) ptr_l;
 	while (len >= sizeof(int))
 	{
 		*(int *) ptr_i++ = *(int *) arr;
 		len -= sizeof(int);
 	}
 	if (len > 0)
-		return (ft_memsetone((void *) ptr_i), c, len);
+		ft_memsetone((void *) ptr_i, c, len);
 	return (b);
 }
